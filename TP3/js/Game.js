95c3a95dc40aab922a89ec53b5e54ec1afb43ef7
Nacho A <nachoalmeida77@gmail.com>
2021-10-27 21:42:38 -0300

    se comento el main y la clase Game

--- TP3/js/Game.js
+++ TP3/js/Game.js
@@ -1,5 +1,5 @@
 class Game {
-    constructor(chronometer, backgrounds, spanLife, spanPoints) {
+    constructor(chronometer, backgrounds, spanLife, spanPoints) { //El nuevo juego recibe el div del cronometro, un arreglo con los fondos y los span de vidas y puntos
         this.chronometer = new Chronometer(chronometer);
         this.backgrounds = backgrounds;
         this.backgrounds[1].appendChild(this.backgrounds[2].returnDiv());
@@ -12,19 +12,19 @@ class Game {
         this.game = null;
     }
 
-    unPauseGame() {
+    unPauseGame() { //Despausar Juego
         this.game = 1;
-        this.chronometer.start();
-        this.animatiosState('running')
+        this.chronometer.start(); //Inicia cronometro
+        this.animationState('running') //Todos los elementos del juego estan "corriendo"
     }
 
-    pauseGame() {
+    pauseGame() { //Pausar el juego
         this.game = 0;
-        this.chronometer.pause();
-        this.animatiosState('paused')
+        this.chronometer.pause(); //Pausa el cronometro
+        this.animationState('paused') //Todos los elementos del juego estan "pausados"
     }
 
-    animatiosState(state) {
+    animationState(state) { //Setea el "state" a todos los elementos del juego
         this.player.animationState(state);
         this.pet.animationState(state);
         for (let i = 0; i < this.backgrounds.length; i++) {
@@ -38,94 +38,94 @@ class Game {
         }
     }
 
-    createEnemies() {
-        const tmp = Math.random();
+    createEnemies() { //Crea los enemigos
+        const tmp = Math.random(); //tmp guarda un numero entre 0 y 1
         let getEnemy = null;
-        if (tmp < 0.5) {
-            getEnemy = new Character(document.createElement("div"), "id", "bear" + tmp, "runBear");
-        } else {
-            getEnemy = new Character(document.createElement("div"), "id", "bird" + tmp, "flyBird");
+        if (tmp < 0.5) { //Si tmp es menor a 0.5..
+            getEnemy = new Character(document.createElement("div"), "id", "bear" + tmp, "runBear"); //Crea un oso
+        } else { //Si tmp es mayor o igual a 0.5..
+            getEnemy = new Character(document.createElement("div"), "id", "bird" + tmp, "flyBird"); //Crea un cuervo
         }
-        this.backgrounds[2].appendChild(getEnemy.returnDiv());
-        this.enemies.push(getEnemy);
+        this.backgrounds[2].appendChild(getEnemy.returnDiv()); //Agrega el enemigo al DOM dentro de backgroundTwo
+        this.enemies.push(getEnemy); //Guarda el enemigo en un arreglo
     }
 
-    createCoins() {
-        const getCoin = new Character(document.createElement("div"), "id", "coin" + Math.random(), "coin");
-        this.backgrounds[2].appendChild(getCoin.returnDiv());
-        this.coins.push(getCoin);
+    createCoins() { //Crea las monedas
+        const getCoin = new Character(document.createElement("div"), "id", "coin" + Math.random(), "coin"); //Crea una moneda
+        this.backgrounds[2].appendChild(getCoin.returnDiv()); //Agrega la moneda al DOM dentro de backgroundTwo
+        this.coins.push(getCoin); //Guarda la moneda en un arreglo
     }
 
-    startGame(value) {
-        this.chronometer.start();
+    startGame(value) { //Inicia el juego, recibe los datos del personaje seleccionado
+        this.chronometer.start(); //Inicia el cronometro que muestra el juego
         this.game = 1;
-        const players = value.split("-");
+        const players = value.split("-"); //Separa por "-" y guarda en un arreglo los datos obtenidos del select del personaje
         for (let i = 0; i < players.length; i++) {
-            const values = players[i].split(","); //[0] id,[1]value,[2]clase,[3]clase,[4]clase
-            const getPlayer = new Player(document.createElement("div"), values[0], values[1], values[2], values[3], values[4], values[5]);
-            this.backgrounds[2].appendChild(getPlayer.returnDiv());
+            const values = players[i].split(","); //Separa por "," y guarda en un arreglo. Obtiene:[0]id,[1]value,[2]clase,[3]clase,[4]clase,[5]clase
+            const getPlayer = new Player(document.createElement("div"), values[0], values[1], values[2], values[3], values[4], values[5]); //Crea el Jugador
+            this.backgrounds[2].appendChild(getPlayer.returnDiv()); //Agrega el jugador al DOM dentro de backgroundTwo
             if (i == 0) {
-                this.player = getPlayer;
+                this.player = getPlayer; //En la primer ubicacion habrá un jugador, por lo que guarda un jugador
             } else {
-                this.pet = getPlayer;
+                this.pet = getPlayer; //En la segunda ubicación habra una mascota, por lo que guarda una mascota
             }
         }
     }
 
     getGame() {
-        return this.game;
+        return this.game; //Devuelve si el juego, p saber si esta iniciado
     }
 
     getLifes() {
-        return this.spanLife.getPoints();
+        return this.spanLife.getPoints(); //Devuelve el numero de vidas
     }
 
-    doneEvent(key) { //Chequea la tecla presionada y ejecuta lo que corresponda
+    doneEvent(key) { //Chequea la tecla presionada, tambien que hayan terminado de ejecutar otra accion, y ejecuta lo que corresponda
         if (key === 'ArrowUp' && (this.player.getJumpDone() && this.pet.getJumpDone()) && this.player.getBendDone()) {
-            this.player.jump();
+            this.player.jump(); //Saltan jugador y mascota
             this.pet.jump();
         }
         if (key === 'ArrowDown' && (this.player.getBendDone() && this.pet.getJumpDone())) {
-            this.player.bend();
+            this.player.bend(); //Se agacha sólo el jugador
         }
     }
 
-
+    //////////////////METODO DE CONTROL DE LOS PERSONAJES DEL JUEGO Y SU INTERACCION////////////////
     gameRun() {
-        for (let i = 0; i < this.enemies.length; i++) {
-            if (this.enemies[i].getId() !== this.spanLife.getLast() && this.collision(this.enemies[i])) {
+        for (let i = 0; i < this.enemies.length; i++) { //Para cada enemigo..
+            if (this.enemies[i].getId() !== this.spanLife.getLast() && this.collision(this.enemies[i])) { //Si el id del enemigo chequeado es distinto al ultimo registrado y hay colision, resto una vida
                 this.spanLife.setPoints(this.spanLife.getPoints() - 1);
                 this.spanLife.setLast(this.enemies[i].getId());
             };
         }
-        if (this.enemies.length > 0) {
-            if (this.enemies[0].getRight() > 900) {
-                this.backgrounds[2].removeChild(this.enemies[0].returnDiv());
-                this.enemies.shift();
+        if (this.enemies.length > 0) { //Si hay algun enemigo..
+            if (this.enemies[0].getRight() > 900) { //Chequeo que el primero del arreglo se salga de la pantalla..
+                this.backgrounds[2].removeChild(this.enemies[0].returnDiv()); //Elimino al enemigo del DOM 
+                this.enemies.shift(); //Elimino al enemigo del arreglo que lo contenia
             }
         }
         for (let i = 0; i < this.coins.length; i++) {
-            let check = this.collision(this.coins[i]);
-            if (this.coins[i].getId() !== this.spanPoints.getLast() && check) {
-                this.spanPoints.setLast(this.coins[i].getId());
-                this.coins[i].addClass('coinShow');
-                let coin = this.coins[i];
-                this.coins.splice(i, 1);
+            let check = this.collision(this.coins[i]); //Chequeo para una moneda si hay colision
+            if (this.coins[i].getId() !== this.spanPoints.getLast() && check) { //Si el id de la moneda es distinto al ultimo registrado y hay colision..
+                this.spanPoints.setLast(this.coins[i].getId()); //guardo el id de la moneda
+                this.coins[i].addClass('coinShow'); //Agrego clase p mostrar la animación correspondiente
+                let coin = this.coins[i]; //Guardo la moneda
+                this.coins.splice(i, 1); //Elimino la moneda del arreglo
                 setTimeout(() => {
-                    this.spanPoints.setPoints(this.spanPoints.getPoints() + 1);
-                    this.backgrounds[2].removeChild(coin.returnDiv());
+                    this.spanPoints.setPoints(this.spanPoints.getPoints() + 1); //Agrego 1 a la cantidad de monedas recolectadas
+                    this.backgrounds[2].removeChild(coin.returnDiv()); //Elimino la moneda del DOM
                 }, 500);
             }
         }
-        if (this.coins.length > 0) {
-            if (this.coins[0].getRight() > 900) {
-                this.backgrounds[2].removeChild(this.coins[0].returnDiv());
-                this.coins.shift();
+        if (this.coins.length > 0) { //Si hay alguna moneda..
+            if (this.coins[0].getRight() > 900) { //Y se sale de la pantalla..
+                this.backgrounds[2].removeChild(this.coins[0].returnDiv()); //Elimino la moneda del DOM
+                this.coins.shift(); //Elimino la moneda del arreglo
             }
         }
     }
 
-    collision(enemy) {
+    collision(enemy) { //Chequea si el jugador colisiona contra algun elemento del juego
         if (this.player.getRight() < enemy.getRight() + enemy.getWidth() && this.player.getRight() + this.player.getWidth() > enemy.getRight() && this.player.getTop() < enemy.getTop() + enemy.getHeight() && this.player.getTop() + this.player.getHeight() > enemy.getTop()) {
             return true;
         } else {
@@ -135,6 +135,6 @@ class Game {
 
     diePlayer() {
         this.player.animationState('running');
-        this.player.die();
+        this.player.die(); //Setea una clase para que efectue la animacion correspondiente y luego pausa el jugador
     }
 }
--- TP3/js/main.js
+++ TP3/js/main.js
@@ -22,7 +22,7 @@ const spanPoints = document.getElementById("points");
 let game = new Game(chronometer, [background, backgroundOne, backgroundTwo], spanLife, spanPoints);
 let gameOverBoolean = false;
 
-/////////////////SALTAR////////////////////////////////
+/////////////////DETECCION DE EVENTOS TECLAS////////////////////////////////
 document.addEventListener('keydown', (e) => {
     if (game.getGame()) { //Chequea si el juego esta corriendo.
         let key = e.key; //Detecta el evento y obtiene el nombre de la tecla presionada
@@ -47,14 +47,15 @@ buttonPlayStop.addEventListener('click', () => {
         document.getElementById("pause").classList.remove('pauseShow'); //Despliega el menu de pausa
         buttonRestart.classList.add('btnTrue'); //Muestra el boton "restart"
         game.pauseGame(); //Pausa el juego
-        timers.clearTimers(); //Reinicia los timers que crean las monedas y enemigos
+        timers.clearTimers(); //Elimina los timers que crean las monedas y enemigos
         clearInterval(timer); //Reinicia el timer que chequea que le queden vidas al jugador
     }
 });
+/////////////////FIN BOTON PAUSA////////////////////////////////
 
 /////////////////BOTON REINICIAR////////////////////////////////
-buttonRestart.addEventListener('click', () => {
-    buttonRestart.classList.remove('btnTrue'); //Quita el boton "restart"
+buttonRestart.addEventListener('click', () => { //Si se cliquea el boton "Restart"
+    buttonRestart.classList.remove('btnTrue'); //Quita el boton "Restart"
     document.getElementById("divSelect").classList.remove('selectTwo'); //Deja de mostrar la cantidad de vidas
     document.getElementById("divPoints").classList.remove('pointsTwo'); //Deja de mostrar la cantidad de monedas recolectadas
     createBackgrouds(); //Redibuja los fondos
@@ -66,7 +67,9 @@ buttonRestart.addEventListener('click', () => {
         gameOverBoolean = false;
     }
 });
+/////////////////FIN BOTON REINICIAR////////////////////////////////
 
+/////////////////METODOS CONTROL GENERAL DEL JUEGO////////////////////////////////////
 function createBackgrouds() { //Crea el fondo del juego
     backgroundTwo = new Animation(document.createElement("div"), 'id', 'backgroundTwo', 'animationBackgroundTwo');
     backgroundOne = new Animation(document.createElement("div"), 'id', 'backgroundOne', 'animationBackgroundOne');
@@ -76,33 +79,31 @@ function createBackgrouds() { //Crea el fondo del juego
     background.appendChild(backgroundOne.returnDiv()); //Inserta el backgroundOne en el background
 }
 
-/////////////////////////////CREAR ENEMIGOS////////////////////////////////////
-function startGame() {
-    const value = document.getElementById("select").value;
-    game.startGame(value);
-    document.getElementById("divSelect").classList.add('selectTwo');
-    document.getElementById("divPoints").classList.add('pointsTwo');
-    timers.startTimers(game);
-    startGameTimer();
+function startGame() { //Iniciar Juego
+    const value = document.getElementById("select").value; //Obtiene y guarda los datos del personaje seleccionado
+    game.startGame(value); //Inicia el juego con el personaje seleccionado
+    document.getElementById("divSelect").classList.add('selectTwo'); //Muestra la cantidad de vidas
+    document.getElementById("divPoints").classList.add('pointsTwo'); //Muestra la cantidad de monedas recolectadas
+    timers.startTimers(game); //Inicia los timers que crean las monedas y enemigos
+    startGameTimer(); //Inicia el timer que chequea que le queden vidas al jugador
 }
 
-function startGameTimer() {
+function startGameTimer() { //Chequea en intervalos de tiempo que le queden vidas al jugador
     timer = setInterval(() => {
-        if (game.getLifes() > 0) {
-            game.gameRun();
-        } else {
-            gameOver();
+        if (game.getLifes() > 0) { //Si al jugador le quedan vidas..
+            game.gameRun(); //El juego sigue su curso
+        } else { //Si no le quedan más vidas..
+            gameOver(); //El juego termina
         }
     }, 50);
 }
 
-function gameOver() {
-    game.pauseGame();
-    game.diePlayer();
-    document.getElementById("pause").classList.remove('pauseShow');
-    gameOverText.classList.add('gameOverShow');
-    buttonRestart.classList.add('btnTrue');
-    //buttonPlayStop.classList.toggle("play");
-    buttonPlayStop.style.display = 'none';
-    gameOverBoolean = true;
+function gameOver() { //Fin del Juego
+    game.pauseGame(); //Se pausa el juego
+    game.diePlayer(); //El jugador muere y se muestra la animación correspondiente
+    document.getElementById("pause").classList.remove('pauseShow'); //Despliega el menu de pausa
+    gameOverText.classList.add('gameOverShow'); //Muestra el mensaje "Game Over"
+    buttonRestart.classList.add('btnTrue'); //Muestra el boton "Restart"
+    buttonPlayStop.style.display = 'none'; //No muestra el boton Play/Pause
+    gameOverBoolean = true; //Asigna true para avisar que el juego se cortó por "Game Over"
 }
