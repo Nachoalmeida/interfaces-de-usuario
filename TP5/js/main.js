5c907401b4af70b8b01f827aaa9324fcb2d4fb0f
alexisleonel1 <alexisleogalvan12@gmail.com>
2021-12-05 18:09:51 -0300

    se agrego interaccion al home

--- TP5/js/main.js
+++ TP5/js/main.js
@@ -13,8 +13,6 @@ btnChatsOn.addEventListener('click', () => {
     }
 });
 
-
-
 const chat1 = document.getElementById("chat1-1");
 const onChat1 = document.getElementById("onChat1").addEventListener('click', ()=>onchat());
 const onChat2 = document.getElementById("onChat2").addEventListener('click', ()=>onchat());;
@@ -36,10 +34,34 @@ function offchat(){
 }
 
 
-
-
 const boxcomment1 = document.getElementById("boxcomment1");
 const post1comment = document.getElementById("post1comment").addEventListener('click', ()=>onComment());
+
+const boxcomment2 = document.getElementById("boxcomment2");
+const post2comment = document.getElementById("post2comment").addEventListener('click', ()=>onComment2());
+
+const boxcomment3 = document.getElementById("boxcomment3");
+const post3comment = document.getElementById("post3comment").addEventListener('click', ()=>onComment3());
+
+function onComment3(){
+    if(boxcomment3.classList.contains('box-comment-off')){
+        boxcomment3.classList.remove('box-comment-off');
+        boxcomment3.classList.add('box-comment-on');
+    }else{
+        boxcomment3.classList.add('box-comment-off');
+        boxcomment3.classList.remove('box-comment-on');
+    }
+}
+
+function onComment2(){
+    if(boxcomment2.classList.contains('box-comment-off')){
+        boxcomment2.classList.remove('box-comment-off');
+        boxcomment2.classList.add('box-comment-on');
+    }else{
+        boxcomment2.classList.add('box-comment-off');
+        boxcomment2.classList.remove('box-comment-on');
+    }
+}
 
 
 function onComment(){
