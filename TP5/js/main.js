0518b0b71bfd783dcab62c05b11264d416481376
alexisleonel1 <alexisleogalvan12@gmail.com>
2021-12-05 13:36:22 -0300

    se creo pantalla de chat1:1

--- TP5/js/main.js
+++ TP5/js/main.js
@@ -12,3 +12,26 @@ btnChatsOn.addEventListener('click', () => {
         listChats.classList.remove('list-chats-on');
     }
 });
+
+
+
+const chat1 = document.getElementById("chat1-1");
+const onChat1 = document.getElementById("onChat1").addEventListener('click', ()=>onchat());
+const onChat2 = document.getElementById("onChat2").addEventListener('click', ()=>onchat());;
+const btnCloseChat = document.getElementById("btnCloseChat");
+
+
+btnCloseChat.addEventListener('click', () => {
+    offchat();
+});
+
+function onchat(){
+    chat1.classList.remove('chat-1-off');
+    chat1.classList.add('chat-1-on');
+}
+
+function offchat(){
+    chat1.classList.remove('chat-1-on');
+    chat1.classList.add('chat-1-off');
+}
+
